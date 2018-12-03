ac163e9fe39db866e1e10efb71e1e57e558ace0e
Zeu <zhz1208@gmail.com>
2018-12-02 19:48:59 -0800

    fixed tons of bugs

--- Surveillance/js/index.js
+++ Surveillance/js/index.js
@@ -1,10 +1,16 @@
 
+//landscape mode
+function testOrientation() {
+  document.getElementById('block_land').style.display = (screen.width>screen.height) ? 'none' : 'block';
+}
+
+
 function resize_video(popup_name) {
-	var video = $("#" + popup_name).find('video').get(0);
-	video.play();
-	setTimeout(function() {
-		video.pause();
-	}, 300);
+    var video = $("#" + popup_name).find('video').get(0);
+    video.play();
+    setTimeout(function() {
+        video.pause();
+    }, 300);
 }
 
 $(function() {
@@ -17,6 +23,18 @@ $(function() {
     $('#carousel').bind('slide.bs.carousel', function(e) {
         $('video').not('.active > div > video').each(function() {
             $(this).get(0).pause();
+        });
+    });
+
+    $('a.carousel-control').click(function() {
+        $('.slide-content video').each(function(index, e) {
+            e.pause();
+        });
+    })
+
+    $('.popup__close').click(function() {
+        $('.slide-content video').each(function(index, e) {
+            e.pause();
         });
     });
 });
